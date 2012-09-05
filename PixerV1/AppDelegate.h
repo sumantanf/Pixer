b30ca54092ac1d911ced5286747a78d2c43b15a7
Shashi Podeti <shashi.k.podeti@gmail.com>
2012-09-05 17:38:23 +0530

    Initial Commit

---
+++ PixerV1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PixerV1
+//
+//  Created by jitu keshri on 9/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PixerV1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PixerV1
+//
+//  Created by jitu keshri on 9/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
